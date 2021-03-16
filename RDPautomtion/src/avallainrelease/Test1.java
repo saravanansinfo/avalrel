9bcc4a83d5709eb6ee315b2cd8f8b2fd1bab0f26
vrishin <vrishin@LAPTOP-VVFFD6DA>
2021-03-16 17:55:33 +0530

    changes

--- RDPautomtion/src/avallainrelease/Test1.java
+++ RDPautomtion/src/avallainrelease/Test1.java
@@ -2,7 +2,8 @@ package avallainrelease;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to eclipse");
 
 	}
