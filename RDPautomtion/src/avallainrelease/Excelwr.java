d8ac4ac5ac96ffd19f3d449e701de394b88b2917
vrishin <vrishin@LAPTOP-VVFFD6DA>
2021-03-26 16:55:34 +0530

    changed

--- RDPautomtion/src/avallainrelease/Excelwr.java
+++ RDPautomtion/src/avallainrelease/Excelwr.java
@@ -1,22 +1,60 @@
 package avallainrelease;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook; 
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-import jxl.Workbook;
 
 public class Excelwr {
 
-	public static void main(String[] args) throws EncryptedDocumentException, InvalidFormatException, IOException 
+	public static void main(String[] args) throws InvalidFormatException, IOException 
 	{
-		FileInputStream fi = new FileInputStream("F:\\RDPAutomation\\RDP_Template_Variation_Matrix.xlsx");
+		// initilize fileInputStream and set path of excel file
+		FileInputStream is = new FileInputStream("F:\\RDPAutomation\\RDPAuto.xls");
+
+			// Create workbook and access file into Workbook
+			Workbook wb = WorkbookFactory.create(is);
+
+			// Get sheet by sending sheet id or sheet name
+			Sheet s = wb.getSheetAt(0);
+			//Sheet s = wb.getSheetAt(“Sheet1”);
+			
+
+			// Create object for Row and pass which row need to be accessed
+			Row r = s.getRow(1);	
+			
+			//Create new cell and set data by passing column number and Cell Value
+			r.createCell(2).setCellValue("Result");
+			
+			r = s.getRow(2);	
+			
+			//Create new cell and set data by passing column number and Cell Value
+			r.createCell(2).setCellValue("PASS");
+			
+			r = s.getRow(3);	
+			
+			//Create new cell and set data by passing column number and Cell Value
+			r.createCell(2).setCellValue("FAIL");
+			
+			//Initilize file outputStream
+		FileOutputStream fOut = new FileOutputStream("F:\\RDPAutomation\\RDPAuto.xls");
+			
+			//Write into workbook
+			wb.write(fOut);
+			
+			//close file
+			fOut.close();
+			is.close();
 		
-		Workbook wb = (Workbook) WorkbookFactory.create(fi);
+		
+		
 		
 		
 		
